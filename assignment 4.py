f295c37b5138a0a53abbef2a17008f914e4883cf
ssathyaa <98035068+ssathyaa@users.noreply.github.com>
2022-01-24 21:03:36 +0530

    Add files via upload

---
+++ assignment 4.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[64]:
+
+
+#function 1:
+print(f"function 1:")
+def dict():
+    a=3445
+    list1=[2,3,"four"]
+    tuple1=(5,6)
+    dict1={3:"three"}
+    set1={1,2,3}
+    print(f"{a} \n{list1}\n{tuple1}\n{dict1}\n{set1} ")
+ 
+dict() 
+
+#function 3:
+print()
+print(f"function 3:")
+def list(a,b):
+     print(f"[{a},{b}]")
+list(32,42)
+
+#function 2:
+print()
+print(f"function 2:")
+def dict1(d):
+    for i in dic.items():
+        print(i)
+d={1:2,3:4}
+dict1(d)
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
